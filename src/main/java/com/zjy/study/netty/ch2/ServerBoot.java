2a80074bd91d294b02763e4d8d09e8cd81e384b3
junyong <zhang.123>
2019-05-22 11:43:05 +0800

    add modules

---
+++ src/main/java/com/zjy/study/netty/ch2/ServerBoot.java
@@ -0,0 +1,10 @@
+package com.zjy.study.netty.ch2;
+
+public class ServerBoot {
+
+    private static final int PORT = 8000;
+
+    public static void main(String[] args) {
+        new Server(PORT).start();
+    }
+}
